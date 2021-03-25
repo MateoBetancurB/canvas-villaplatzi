ebd5027e30d1296018e8fa5eec9cfe8918c35f71
Mateo B <mateobetancurb@gmail.com>
2021-03-25 12:58:29 -0500

    cambios 1.5

--- villa.js
+++ villa.js
@@ -1,22 +1,43 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 var mapa = "tile.png";
-var animal1 = "vaca.png";
+// var animal1 = "vaca.png";
+// var animal2 = "cerdo.png";
+// var animal3 = "pollo.png";
 
 var fondo = new Image();
 fondo.src = mapa;
 fondo.addEventListener("load", dibujar);
 
 var vaca = new Image();
-vaca.src = animal1;
-vaca.addEventListener("load", dibujar);
+vaca.src = "vaca.png";
+vaca.addEventListener("load", dibujarVacas);
 
+var cerdo = new Image();
+cerdo.src = "cerdo.png";
+cerdo.addEventListener("load", dibujarCerdos);
 
+var pollo = new Image();
+pollo.src = "pollo.png";
+pollo.addEventListener("load", dibujarPollos);
 
 function dibujar() {
     papel.drawImage(fondo, 0, 0);
-    papel.drawImage(vaca, 100, 100);
 }
+
+function dibujarVacas() {
+    papel.drawImage(vaca, 10, 10);
+}
+
+function dibujarCerdos() {
+    papel.drawImage(cerdo, 10, 300);
+}
+
+function dibujarPollos() {
+    papel.drawImage(pollo, 300, 150);
+}
+
+
 
 function aleatorio(min, maxi) {
     var resultado;
