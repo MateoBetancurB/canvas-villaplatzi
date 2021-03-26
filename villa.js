f86be49a30f8578afe02238cef37835849c8bfa0
Mateo B <mateobetancurb@gmail.com>
2021-03-26 09:39:10 -0500

    cambios 2.0 - problema resuelto

--- villa.js
+++ villa.js
@@ -1,43 +1,42 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
-var mapa = "tile.png";
-// var animal1 = "vaca.png";
-// var animal2 = "cerdo.png";
-// var animal3 = "pollo.png";
 
-var fondo = new Image();
-fondo.src = mapa;
-fondo.addEventListener("load", dibujar);
+var fondo = {
+    url: "tile.png",
+    cargaOK: false
+};
+var vaca = {
+    url: "vaca.png",
+    cargaOK: false
+};
 
-var vaca = new Image();
-vaca.src = "vaca.png";
-vaca.addEventListener("load", dibujarVacas);
 
-var cerdo = new Image();
-cerdo.src = "cerdo.png";
-cerdo.addEventListener("load", dibujarCerdos);
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
 
-var pollo = new Image();
-pollo.src = "pollo.png";
-pollo.addEventListener("load", dibujarPollos);
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+function cargarFondo() {
+    fondo.cargaOK = true;
+    dibujar();
+}
+
+function cargarVacas() {
+    vaca.cargaOK = true;
+    dibujar();
+
+}
+
 
 function dibujar() {
-    papel.drawImage(fondo, 0, 0);
+
+    if (fondo.cargaOK) {
+        papel.drawImage(fondo.imagen, 0, 0);
+    }
 }
-
-function dibujarVacas() {
-    papel.drawImage(vaca, 10, 10);
-}
-
-function dibujarCerdos() {
-    papel.drawImage(cerdo, 10, 300);
-}
-
-function dibujarPollos() {
-    papel.drawImage(pollo, 300, 150);
-}
-
-
 
 function aleatorio(min, maxi) {
     var resultado;
